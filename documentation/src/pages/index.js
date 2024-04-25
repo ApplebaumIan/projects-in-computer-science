3fc828568a12a661f8b7b5c04413888ab0cbc179
Ian Applebaum <Ian.tyler@electricbluefish.com>
2024-04-25 11:40:15 -0400

    Live Demos Spring 2024

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -27,57 +27,57 @@ function isTimeBetween(startDate, endDate) {
 }
 
 export function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container" style={{zIndex:100}}>
-          <div className={"row"}>
-              <div className={"col"}>
-                  <h1 className="hero__title">{siteConfig.customFields.course_number} <br/>
-                      {siteConfig.title}
-                  </h1>
-                  <p className="hero__subtitle">{siteConfig.customFields.semester} {siteConfig.tagline}</p>
-                  <div className={"col"}>
-                      {/* TODO: Change me to your project's tutorial*/ }
-                      <Link
-                          className="button button--secondary button--lg margin--md"
-                          to="#office-hours-professor-applebaum">
-                          Student Office Hours ️👨‍🏫
-                      </Link>
-                      <Link
-                          className="button button--secondary button--lg margin--md"
-                          to="#class-discord">
-                          Class Discord Server 💬
-                      </Link>
-                      <Link
-                          className="button button--secondary button--lg margin--md"
-                          to="#course-schedule">
-                          Course Schedule 📆
-                      </Link>
-                      <Link
-                          className="button button--secondary button--lg margin--md"
-                          to="/tutorial/intro">
-                          Docusaurus Tutorial 🦖
-                      </Link>
-                  </div>
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container" style={{zIndex:100}}>
+                <div className={"row"}>
+                    <div className={"col"}>
+                        <h1 className="hero__title">{siteConfig.customFields.course_number} <br/>
+                            {siteConfig.title}
+                        </h1>
+                        <p className="hero__subtitle">{siteConfig.customFields.semester} {siteConfig.tagline}</p>
+                        <div className={"col"}>
+                            {/* TODO: Change me to your project's tutorial*/ }
+                            <Link
+                                className="button button--secondary button--lg margin--md"
+                                to="#office-hours-professor-applebaum">
+                                Student Office Hours ️👨‍🏫
+                            </Link>
+                            <Link
+                                className="button button--secondary button--lg margin--md"
+                                to="#class-discord">
+                                Class Discord Server 💬
+                            </Link>
+                            <Link
+                                className="button button--secondary button--lg margin--md"
+                                to="#course-schedule">
+                                Course Schedule 📆
+                            </Link>
+                            <Link
+                                className="button button--secondary button--lg margin--md"
+                                to="/tutorial/intro">
+                                Docusaurus Tutorial 🦖
+                            </Link>
+                        </div>
 
-              </div>
+                    </div>
 
-              {/*{docusaurusConfig.customFields.is_pdf ? <></> : <WatchLiveDemo/>}*/}
+                    {docusaurusConfig.customFields.is_pdf ? <></> : <WatchLiveDemo/>}
 
 
-        </div>
-      </div>
-        {/*<BrowserOnly  fallback={<div>Loading...</div>} >*/}
-        {/*    {() => {*/}
-        {/*        const AnimatedBackground =*/}
-        {/*            require('../components/AnimatedBackground/index').AnimatedBackground;*/}
-        {/*        return <AnimatedBackground/>;*/}
-        {/*    }}*/}
-        {/*</BrowserOnly>*/}
+                </div>
+            </div>
+            {/*<BrowserOnly  fallback={<div>Loading...</div>} >*/}
+            {/*    {() => {*/}
+            {/*        const AnimatedBackground =*/}
+            {/*            require('../components/AnimatedBackground/index').AnimatedBackground;*/}
+            {/*        return <AnimatedBackground/>;*/}
+            {/*    }}*/}
+            {/*</BrowserOnly>*/}
 
-    </header>
-  );
+        </header>
+    );
 }
 
 function WatchLiveDemo() {
@@ -93,8 +93,8 @@ function WatchLiveDemo() {
         <div className={"card__header row"}>
             <div className={"col col--9"}>
                 <h2>Watch Live 🔴</h2>
-                <h3>December 11th 10:30am to 5:00pm EST on YouTube</h3>
-                <p>64 Students, 9 projects, come see Temple University students present their 17 weeks worth of work live on YouTube. This semester projects range from embedded systems projects, augmented reality, unique machine learning experiences to help improve an underserved community's daily life and so much more.</p>
+                <h3>April 29th 9:30am to 5:00pm EST on YouTube</h3>
+                <p>We have a lot to share this semester. 6 ambitious projects dealing with AI, Accessibility, Computer Vision, IoT, Embedded Systems, Gaming, and Computer Science Education. <b>Come see what Temple University's class of 2024 has to offer.</b></p>
 
             </div>
             <div className={"col col--3"}>
@@ -104,9 +104,9 @@ function WatchLiveDemo() {
         </div>
         <div className={"card__body"}>
             <Tabs queryString="section">
-                <TabItem value="701" label={<>Section 701<br/>10:30-11:50am EST</>}>
-                    <Figure caption={"Final Demos Fall Semester 2023 Live Stream"} subcaption={"Section 701"}>
-                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/Wge6Wd8ctRI?si=uA_0c1Y_gDTvJ-jm"
+                <TabItem value="001" label={<>Section 001<br/>9:30-10:50am EST<br/>📍SERC 214</>}>
+                    <Figure caption={"Final Demos Spring Semester 2024 Live Stream"} subcaption={"Section 001"}>
+                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/7Hc27xvGuj4"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen></iframe>
@@ -116,29 +116,13 @@ function WatchLiveDemo() {
                             <summary className={"button button--outline button--primary margin-bottom--lg"}>
                                 Click Here For the Demo Lineup!
                             </summary>
-                            <YouTubeVideoDescription videoId={"Wge6Wd8ctRI"}/>
+                            <YouTubeVideoDescription videoId={"7Hc27xvGuj4"}/>
                         </details>
                     </div>
                 </TabItem>
-                <TabItem value="002" label={<>Section 002<br/>2:00pm to 3:20pm EST</>} default={isTimeBetween(section002Start,section002End)}>
-                    <Figure caption={"Final Demos Fall Semester 2023 Live Stream"} subcaption={"Section 002"}>
-                    <iframe width="100%" height="615" src="https://www.youtube.com/embed/xYrKWJfFlUc?si=9Aw-8zQbGpXQobEm"
-                            title="YouTube video player" frameBorder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowFullScreen></iframe>
-                    </Figure>
-                    <div className={"col"}>
-                        <details>
-                            <summary className={"button button--outline button--primary margin-bottom--lg"}>
-                                Click Here For the Demo Lineup!
-                            </summary>
-                            <YouTubeVideoDescription videoId={"xYrKWJfFlUc"}/>
-                        </details>
-                    </div>
-                </TabItem>
-                <TabItem value="003" label={<>Section 003<br/>3:40pm to 5:00pm EST</>} default={isTimeBetween(section003Start,section003End)}>
-                    <Figure caption={"Final Demos Fall Semester 2023 Live Stream"} subcaption={"Section 003"}>
-                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/XxRJPMJZ6Fk?si=AuK6OV1CxdGTYNv3"
+                <TabItem value="002" label={<>Section 002<br/>12:30pm to 1:50pm EST<br/>📍SERC 306</>} default={isTimeBetween(section002Start,section002End)}>
+                    <Figure caption={"Final Demos Spring Semester 2024 Live Stream"} subcaption={"Section 002"}>
+                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/w5BaWx_9U6U"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 allowFullScreen></iframe>
@@ -148,7 +132,23 @@ function WatchLiveDemo() {
                             <summary className={"button button--outline button--primary margin-bottom--lg"}>
                                 Click Here For the Demo Lineup!
                             </summary>
-                            <YouTubeVideoDescription videoId={"XxRJPMJZ6Fk"}/>
+                            <YouTubeVideoDescription videoId={"w5BaWx_9U6U"}/>
+                        </details>
+                    </div>
+                </TabItem>
+                <TabItem value="003" label={<>Section 003<br/>3:30pm to 4:00pm EST<br/>📍SERC 306</>} default={isTimeBetween(section003Start,section003End)}>
+                    <Figure caption={"Final Demos Spring Semester 2024 Live Stream"} subcaption={"Section 003"}>
+                        <iframe width="100%" height="615" src="https://www.youtube.com/embed/Y7IkoT6fJuc"
+                                title="YouTube video player" frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                allowFullScreen></iframe>
+                    </Figure>
+                    <div className={"col"}>
+                        <details>
+                            <summary className={"button button--outline button--primary margin-bottom--lg"}>
+                                Click Here For the Demo Lineup!
+                            </summary>
+                            <YouTubeVideoDescription videoId={"Y7IkoT6fJuc"}/>
                         </details>
                     </div>
                 </TabItem>
@@ -158,19 +158,19 @@ function WatchLiveDemo() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-        title={`Syllabus`}
-        description="Professor Applebaum's Capstone Course Syllabus.">
-        <HomepageHeader/>
-        <main>
-            <div style={{zIndex:100000}}>
-                <MDXContent>
-                    <SyllabusPage/>
-                </MDXContent>
-            </div>
-        </main>
-    </Layout>
-  );
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Syllabus`}
+            description="Professor Applebaum's Capstone Course Syllabus.">
+            <HomepageHeader/>
+            <main>
+                <div style={{zIndex:100000}}>
+                    <MDXContent>
+                        <SyllabusPage/>
+                    </MDXContent>
+                </div>
+            </main>
+        </Layout>
+    );
 }
