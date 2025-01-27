d1eb678553bcbc4c9b33ceea00afd6c70ed642ef
Ian Applebaum <Ian.tyler@electricbluefish.com>
2025-01-27 10:23:56 -0500

    hotfix add TAs

--- documentation/src/components/Instructor/Instructor.jsx
+++ documentation/src/components/Instructor/Instructor.jsx
@@ -42,6 +42,11 @@ export default function Instructor() {
                             Lecture: Wed 4:00-4:50 PM
                         </li>
                     </ul>
+                    <h2>Teaching Assistants</h2>
+                    <ul>
+                        <li>Kyle Dragon Lee: <a href={"mailto:kyle.d.lee@temple.edu"}>kyle.d.lee@temple.edu</a></li>
+                        <li>Thanh Nguyen: <a href={"mailto:thanh.png@temple.edu"}>thanh.png@temple.edu</a></li>
+                    </ul>
                     <OfficeHours />
                 </div>
 
