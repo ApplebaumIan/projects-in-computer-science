33132c4d2155e05952641e5f47ed2e33bb50e2dc
Ian Applebaum <Ian.tyler@electricbluefish.com>
2025-12-01 23:51:50 -0500

    Update demo lineup and showcase details;

--- documentation/src/pages/showcase/_components/ShowcaseCard/index.tsx
+++ documentation/src/pages/showcase/_components/ShowcaseCard/index.tsx
@@ -64,7 +64,7 @@ function getCardImage(user: User): string {
     // TODO make it configurable
     `https://slorber-api-screenshot.netlify.app/${encodeURIComponent(
       user.useDocsAsPreview ? user.website : user.documentation,
-    )}/showcase`
+    )}/showcase/_4234234`
   );
 }
 
