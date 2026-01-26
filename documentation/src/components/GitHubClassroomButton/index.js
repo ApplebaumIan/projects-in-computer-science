f585fd032a3e905d959226aacfb514bd5cbb4793
Ian Tyler Applebaum <9451941+ApplebaumIan@users.noreply.github.com>
2026-01-26 02:04:48 +0000

    Enhance GitHub Classroom components to support dynamic links and add Piggyback Learning note

--- documentation/src/components/GitHubClassroomButton/index.js
+++ documentation/src/components/GitHubClassroomButton/index.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Link from '@docusaurus/Link';
 
-const GitHubClassroomButton = () => {
+const GitHubClassroomButton = ({ link }) => {
   const [githubClassroomLink, setGithubClassroomLink] = useState('');
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const link =
-        window.location.hostname === 'capstone.ianapplebaum.com'
+      const finalLink = link
+        ? link
+        : window.location.hostname === 'capstone.ianapplebaum.com'
           ? 'https://classroom.github.com/a/hUCYbovh'// capstone
           : 'https://classroom.github.com/a/OnZTWK0I'; // software design
-      setGithubClassroomLink(link);
+      setGithubClassroomLink(finalLink);
     }
-  }, []);
+  }, [link]);
 
   return (
     <div className={'col button_group'}>
--- documentation/src/components/GitHubClassroomLink/index.js
+++ documentation/src/components/GitHubClassroomLink/index.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
-const GitHubClassroomLink = () => {
+const GitHubClassroomLink = ({ link }) => {
   const [githubClassroomLink, setGithubClassroomLink] = useState('');
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      const link =
-        window.location.hostname === 'capstone.ianapplebaum.com'
+      const finalLink = link
+        ? link
+        : window.location.hostname === 'capstone.ianapplebaum.com'
           ? 'https://classroom.github.com/a/hUCYbovh' // capstone
           : 'https://classroom.github.com/a/OnZTWK0I';// software design
-      setGithubClassroomLink(link);
+      setGithubClassroomLink(finalLink);
     }
-  }, []);
+  }, [link]);
 
   return <a href={githubClassroomLink}>Click here to join the GitHub Classroom and get the starter project</a>;
 };
