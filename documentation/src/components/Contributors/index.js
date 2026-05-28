014962f9b53356e16cb30e7d7e19d11f1394baf3
Ian Applebaum <Ian.tyler@electricbluefish.com>
2026-05-27 20:00:05 -0400

    Add project members and contributors section to carousel

--- documentation/src/components/Contributors/index.js
+++ documentation/src/components/Contributors/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styles from './styles.module.css';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
@@ -10,20 +10,38 @@ export default function Contributors({ orgName, projectName: projectNameProp , g
     let organizationName = orgName || orgNameConfig;
     let projectName = projectNameProp || projectNameConfig;
     if (githubURLProp) {
-        organizationName = githubURLProp.split('github.com/')[1].split('/')[0];
-        projectName = githubURLProp.split('github.com/')[1].split('/')[1];
+        try {
+            const url = new URL(githubURLProp);
+            const [orgFromUrl, repoFromUrl] = url.pathname.replace(/^\/+/, '').split('/');
+            if (orgFromUrl && repoFromUrl) {
+                organizationName = orgFromUrl;
+                projectName = repoFromUrl;
+            }
+        } catch (error) {
+            // Keep provided/default org and project if URL parsing fails.
+        }
     }
+
+    const repoKey = useMemo(
+        () => `${organizationName}/${projectName}`,
+        [organizationName, projectName],
+    );
+
+    useEffect(() => {
+        setImageError(false);
+    }, [repoKey]);
+
     const handleImageError = () => {
         setImageError(true);
     };
 
     const contributorsImageSrc = imageError
         ? 'https://via.placeholder.com/400x100/f0f0f0/666666?text=Contributors+Not+Available'
-        : `https://contrib.rocks/image?columns=${columns}&repo=${organizationName}/${projectName}`;
+        : `https://contrib.rocks/image?columns=${columns}&repo=${repoKey}`;
 
     const linkHref = imageError
         ? 'tutorial/tutorial-basics/set-environment-variables'
-        : `https://github.com/${organizationName}/${projectName}/graphs/contributors`;
+        : `https://github.com/${repoKey}/graphs/contributors`;
 
     return <div className={styles.contributors}>
         <a href={linkHref}>
