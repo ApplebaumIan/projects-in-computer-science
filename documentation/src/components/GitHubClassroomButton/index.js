bd37eb9f64ff0d150edd44f7d4d6a9b6915eb163
Ian Applebaum <Ian.tyler@electricbluefish.com>
2026-09-13 22:47:04 -0400

    🖕MS. We made our own GitHub Classroom.

--- documentation/src/components/GitHubClassroomButton/index.js
+++ documentation/src/components/GitHubClassroomButton/index.js
@@ -9,7 +9,7 @@ const GitHubClassroomButton = ({ link }) => {
       const finalLink = link
         ? link
         : window.location.hostname === 'capstone.ianapplebaum.com'
-          ? 'https://classroom.github.com/a/D6hMaIHO'// capstone
+          ? 'https://capstone-classroom.ianapplebaum.com/join/jg2dckqtfkjsgzvsb4gjr0ynzdoqytkw'// capstone
           : 'https://classroom.github.com/a/OnZTWK0I'; // software design
       setGithubClassroomLink(finalLink);
     }
